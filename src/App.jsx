e16e7a8c94d28e93378aed3d7091a11f59b215ac
Abizahir Romero <abizahir.romero@hotmail.com>
2022-07-23 00:29:58 -0600

    Modified path redirect

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   return ( 
       <Routes>
         <Route path="/*" element={<MainContainer />} />
-        <Route path="r/:shortedUrl" element={<Redirect />} />
+        <Route path="/r/:shortedUrl" element={<Redirect />} />
       </Routes>
   )
 }
