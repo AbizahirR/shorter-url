d3f292a4b348126e6e433bc899b3ab1ecf48e063
Abizahir Romero <abizahir.romero@hotmail.com>
2022-07-23 00:28:12 -0600

    Redirect modified

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import MainContainer from "./routes/MainContainer"
 import Redirect from "./routes/Redirect"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
 
 function App() {
   
-  return (
-    <BrowserRouter>
+  return ( 
       <Routes>
         <Route path="/*" element={<MainContainer />} />
         <Route path="r/:shortedUrl" element={<Redirect />} />
       </Routes>
-    </BrowserRouter>
   )
 }
 
